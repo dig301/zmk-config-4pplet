a439315e14da96cdd7265b4f8cf1f9a1e9aaf558
joao <joao25109@gmail.com>
2026-03-20 22:37:38 +0100

    Fix pre_dt_board.cmake syntax error

--- config/boards/4pplet/cyber60/pre_dt_board.cmake
+++ config/boards/4pplet/cyber60/pre_dt_board.cmake
@@ -2,17 +2,4 @@
 # Copyright (c) 2022 Stefan Sundin (4pplet)
 # SPDX-License-Identifier: MIT
 
-BOARD_ROOT := ..
-include(${CMAKE_CURRENT_LIST_DIR}/board.cmake)
-
-add_custom_target(
-    pre_dt_target
-    COMMAND ${CMAKE_COMMAND}
-        -DZEPHYR_BASE=${ZEPHYR_BASE}
-        -DTOOLCHAIN_ROOT=${TOOLCHAIN_ROOT}
-        -DCMAKE_SYSTEM_NAME=Zephyr
-        -DCMAKE_SYSTEM_PROCESSOR=cortex-m4
-        -DCFG_TREE=${ZEPHYR_BASE}/include/dt-bindings
-    DEPENDS ${ZEPHYR_BASE}/include/dt-bindings
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/use_dts
-)
+set(BOARD_ROOT ${CMAKE_CURRENT_LIST_DIR}/..)
